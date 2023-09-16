1c9d143ee7b4ddec5a147031187086cadb51606c
Silambarasan <msilambarasan98@gmail.com>
2023-09-16 10:27:02 +0530

    first commit

---
+++ Assignment 5/Calc/Calc/Arithmatic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    public class Arithmatic
+    {
+        public double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public double Sub(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public double Mul(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public double Div(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
---
+++ Assignment 5/Calc/Calc/TestCalc.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    [TestFixture]
+    public class TestCalc
+    {
+        [Test]
+        public void AddTest()
+        {
+            Arithmatic calculator = new Arithmatic();
+            double num1 = 10;
+            double num2 = 10;
+
+            double result = num1 + num2;
+
+            Assert.AreEqual(result, calculator.Add(10, 10));
+        }
+
+        [Test]
+        public void SubTractTest()
+        {
+            Arithmatic calculator = new Arithmatic();
+            double num1 = 10;
+            double num2 = 10;
+
+            double result = num1 - num2;
+
+            Assert.AreEqual(result, calculator.Sub(10, 10));
+        }
+
+        [Test]
+        public void DivideTest()
+        {
+            Arithmatic calculator = new Arithmatic();
+            double num1 = 10;
+            double num2 = 10;
+
+            double result = num1 / num2;
+
+            Assert.AreEqual(result, calculator.Div(10, 10));
+        }
+
+        [Test]
+        public void MultiplyTest()
+        {
+            Arithmatic calculator = new Arithmatic();
+            double num1 = 10;
+            double num2 = 10;
+
+            double result = num1 * num2;
+
+            Assert.AreEqual(result, calculator.Mul(10, 10));
+        }
+    }
+}
